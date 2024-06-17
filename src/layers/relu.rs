7b3dc47dced6fa35818ec1ea0afd52c08d8f904d
pml68 <tutorialmester@gmail.com>
2024-06-17 16:11:33 +0200

    refactor: simplify ReLU code (delete additional function)

--- src/layers/relu.rs
+++ src/layers/relu.rs
@@ -5,21 +5,13 @@ pub struct ActivationReLU {
     pub output: Option<Array2<f64>>,
 }
 
-fn relu(n: f64) -> f64 {
-    if n < 0. {
-        0.
-    } else {
-        n
-    }
-}
-
 impl ActivationReLU {
     pub fn new() -> Self {
         Self { output: None }
     }
 
     pub fn forward(&mut self, inputs: Array2<f64>) {
-        self.output = Some(inputs.mapv(|c| relu(c)));
+        self.output = Some(inputs.mapv(|c| if c < 0. { 0. } else { c }));
     }
 }
 
